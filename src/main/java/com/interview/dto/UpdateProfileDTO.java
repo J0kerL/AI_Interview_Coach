ac382869e4aa2328032ad85bacdfd14ece4f298a
Diamond <2210054954@qq.com>
2026-06-08 15:10:15 +0800

    refactor(dto): 优化 UpdateProfileDTO 字段可选逻辑

--- src/main/java/com/interview/dto/UpdateProfileDTO.java
+++ src/main/java/com/interview/dto/UpdateProfileDTO.java
@@ -1,12 +1,12 @@
 package com.interview.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
  * 修改用户信息请求DTO
+ * 所有字段均可选，仅修改需要变更的字段
  *
  * @Author Diamond
  * @Create 2026/6/4
@@ -15,19 +15,19 @@ import lombok.Data;
 public class UpdateProfileDTO {
 
     /**
-     * 邮箱
+     * 邮箱（可选，不传则不修改）
      */
-    @Email(message = "邮箱格式不正确")
+    @Pattern(regexp = "^$|^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$", message = "邮箱格式不正确")
     private String email;
 
     /**
-     * 手机号
+     * 手机号（可选，不传则不修改）
      */
-    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    @Pattern(regexp = "^$|^1[3-9]\\d{9}$", message = "手机号格式不正确")
     private String phone;
 
     /**
-     * 昵称
+     * 昵称（可选，不传则不修改）
      */
     @Size(max = 100, message = "昵称长度不能超过100个字符")
     private String nickname;
